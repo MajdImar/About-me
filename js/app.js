aee0d30b620b2967f899e7cd51cd0140f89a3aa0
Majd Imar <majd2012995@gmail.com>
2020-02-21 21:31:32 +0200

    edit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict'
 var score = 0;
 var name1 = prompt("What is your name?");
-console.log(name1);
+
 alert(`Hello,Salut in all languages welcome to My webpage ${name1}`);
 
 function q1(){
@@ -9,11 +9,11 @@ function q1(){
 
 var q1 = prompt("Do you think I belive in Alien?").toLowerCase();
 if (q1 === 'yes' || q1 === 'y') {
-   console.log(q1)
+ 
    score = score + 1;
    alert('oui you are correct');
 } else if (q1 === 'no' || q1 === 'n') {
-   console.log(q1)
+
    alert('Non I am sorry');
 }
 else {
@@ -26,11 +26,11 @@ q1();
 function q2(){
 var q2 = prompt("Do you think that I like Mansef").toLowerCase();
 if (q2 === 'yes' || q2 === 'y') {
-   console.log(q2)
+
    alert('oui you are correct'); score = score + 1;
 }
 else if (q2 === 'no' || q2 === 'n') {
-   console.log(q2)
+  
    alert('Non I am sorry')
 }
 else {
@@ -46,13 +46,13 @@ function q3(){
 
 var q3 = prompt("Do you think that I can speak French?").toLowerCase();
 if (q3 === 'yes' || q3 === 'y') {
-   console.log(q3);
+ 
    score = score + 1;
    alert('oui you are correct');
 }
 
 else if (q3 === 'no' || q3 === 'n') {
-   console.log(q3)
+  
    alert('Non I am sorry');
 } else {
    alert('oh!try to put yes/y or no/n');
@@ -67,11 +67,11 @@ function q4(){
 
 var q4 = prompt("Do you think that I play Piano").toLowerCase();
 if (q4 === 'yes' || q4 === 'y') {
-   console.log(q4)
+ 
 
    alert('Non I am sorry');
 } else if (q4 === 'no' || q4 === 'n') {
-   console.log(q4)
+
    alert('oui you are correct');
    score = score + 1;
 }
@@ -88,11 +88,11 @@ function q5(){
 var q5 = prompt("Do you think that I travel a lot?").toLowerCase();
 
 if (q5 === 'yes' || q5 === 'y') {
-   console.log(q5)
+
    alert('Non I am sorry');
 }
 else if (q5 === 'no' || q5 === 'n') {
-   console.log(q5)
+ 
    alert('oui you are correct');
    score = score + 1;
 }
